9428891682ad14f8148251d6dc70d7b2e29374bd
Samad <samad.damas@hotmail.com>
2022-01-21 21:05:29 -0300

    Created the player class with its attributes (variables)

---
+++ game/player.py
@@ -0,0 +1,15 @@
+
+
+class Player:
+
+    #Building the constructor of the class with its attributes
+    def __init__(self):
+
+        self.score = 0
+        self.bet = None
+    
+    #Guess if the next card will be higher or lower
+    def guess(self):
+
+        x = "variable just so the program doesn't think there's an error with the function"
+        y = "Matias, you can erase these both (x and y), and finish the function"
