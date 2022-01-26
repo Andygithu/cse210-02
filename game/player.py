d4a122d4de81a89cb9fafc994630fb71246334d0
Samad <samad.damas@hotmail.com>
2022-01-26 20:10:28 -0300

    Tried working on director and player class

--- game/player.py
+++ game/player.py
@@ -5,11 +5,11 @@ class Player:
     #Building the constructor of the class with its attributes
     def __init__(self):
 
-        self.score = 0
+        self.score = 300
         self.bet = None
     
     #Guess if the next card will be higher or lower
     def guess(self):
 
-        x = "variable just so the program doesn't think there's an error with the function"
-        y = "Matias, you can erase these both (x and y), and finish the function."
+        guess = input("Higher or lower? [h/l] ")
+        self.bet = guess
